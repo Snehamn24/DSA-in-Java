73e906262b31e1bcb903f9e89f45aff0f968a098
Snehamn24 <snehamn216@gmail.com>
2025-09-21 20:26:54 +0530

    Added Bubble sort logic

--- Array/bubble_sort.java
+++ Array/bubble_sort.java
@@ -34,17 +34,17 @@ d . Total Comparisons
         }
         System.out.println();
     
-        //Bubble sort
+        //Bubble sort 
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=0;j<n-i-1;j++)
             {
-                if(arr[i]>arr[j])
+                if(arr[j]>arr[j+1])
                 {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
