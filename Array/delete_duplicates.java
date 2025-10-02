3de753f9485b3a1e76d512cce816c9c51e5bbc4b
Snehamn24 <snehamn216@gmail.com>
2025-10-02 18:54:16 +0530

    Deleting the duplicate elements in the array

---
+++ Array/delete_duplicates.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class delete_duplicates{
+    public static int[] removeDuplicatesUsingSet(int[] arr)
+    {
+        HashSet<Integer> uniqueElements = new HashSet<>();
+        for(int ele : arr)
+        {
+            uniqueElements.add(ele);
+        }
+
+        //converting the hashset back to the the array
+        int[] uniqueArray = new int[uniqueElements.size()];
+        int i=0;
+        for(int element : uniqueElements)
+        {
+            uniqueArray[i++] = element;
+        }
+        return uniqueArray;
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        int[] original_array = {1,2,2,3,4,4,4,5,5};
+        int[] unique_array = removeDuplicatesUsingSet(original_array);
+        System.out.println(Arrays.toString(unique_array));
+    }
+}
