297aa93b93593cb7106b8a7baaeb42bff4fcef0b
Snehamn24 <snehamn216@gmail.com>
2025-10-14 16:40:24 +0530

    Fibonacci Series using the recursion

---
+++ Array/fibonacci.java
@@ -0,0 +1,29 @@
+class fibonacci
+{
+    public static int fib(int n)
+    {
+
+        if(n==0)
+        {
+            return 1;
+        }
+        else if(n==1)
+        {
+            return 1;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+        }
+
+    }
+    public static void main(String[] args)
+    {
+       int n = 5;
+       //printing the sequence
+
+       for(int i=0;i<n;i++)
+       {
+        System.out.print(fib(i)+" ");
+       }
+    }
+}
